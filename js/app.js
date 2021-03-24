e74a26920885bee28118c8640b0ee6ed0244fb1d
anwarmontasir <anwarmontasir@gmail.com>
2021-03-24 10:16:00 -0700

    add Game.js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const game = new Game();
+
+const beginGame = document.getElementById('begin-game');
+
+beginGame.addEventListener('click', e => {
+    e.target.style.display = 'none';
+    document.getElementById('play-area').style.opacity = '1';
+    game.startGame();
+})
