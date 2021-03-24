adb523aa14c881902fa0f6d6802c3f7fd026aa58
anwarmontasir <anwarmontasir@gmail.com>
2021-03-24 10:28:52 -0700

    Game.js

---
+++ js/Game.js
@@ -0,0 +1,17 @@
+class Game {
+    constructor() {
+        this.board = new Board();
+        this.players = this.createPlayers();
+        this.ready = false;
+    }
+
+    createPlayers() {
+        const player1 = new Player('player1', '#e15258', 1, true);
+        const player2 = new Player('player2', '#e59a13', 2);
+        return [player1, player2];
+    }
+
+    startGame() {
+        
+    }
+}
